6df162cfbbd57f9b2b774c7f1ac795afd7fcf9d4
Radmir Hossain <rmh6596@rit.edu>
2022-05-15 00:05:11 -0400

    Tiles should now generate numbers and resources as stated by the rules. NOT TESTED

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 //use rand::Rng;
 use colored::Colorize;      // Random
 use rand::seq::SliceRandom; // Colors
+use rand::Rng;              // Random Number Generator
 use std::fs::File;          // Files
 use std::io::prelude::*;    // Standard I/O
 
@@ -14,7 +15,8 @@ use std::io::prelude::*;    // Standard I/O
 // NOTE: X-Y Coordinate for the tiles in the "map" string are defined by placing the
 //       cursor to the left of the corresponding resource bracket "[".
 ////////////////////////////////////////////////////////////////////////////////////
-fn color_tile(offset: [i32; 2], map: &mut String) {
+fn color_tile(offset: [i32; 2], map: &mut String, resource: String) {
+/*
     let possible_resources = vec![
         // Resource Symbols    RGB Values
         'S', // Sheep ------> (140, 181, 14)
@@ -23,11 +25,9 @@ fn color_tile(offset: [i32; 2], map: &mut String) {
         'C', // Brick ------> (223, 97, 40)
         'O', // Ore   ------> (159, 165, 161)
     ];
-    //Generating random seed
-    let mut rng = rand::thread_rng();
-
+*/
     // Randomly choosing a resource
-    let resource_symbol = possible_resources.choose(&mut rng).unwrap();
+    let resource_symbol = resource;
     // Places the resource symbol across the particular tile
     for i in (-2 as i32)..3 {
         let idx = ((offset[0] + i - 1) * 70 + (offset[1] + i.abs() - 1)) as usize;
@@ -43,13 +43,11 @@ fn color_tile(offset: [i32; 2], map: &mut String) {
 /// generated number on the center of the tile.
 /// Last Modified: 5/14/2022
 ///////////////////////////////////////////////////////////////////////////////////////
-fn enumerate_tile(offset: [i32; 2], map: &mut String){
-    let valid_tile_numbers = vec!["02","03","04","05","!6","!8","09","10","11","12"];
+fn enumerate_tile(offset: [i32; 2], map: &mut String, number: String){
+    // let valid_tile_numbers = vec!["02","03","04","05","!6","!8","09","10","11","12"];
     
-    //Generating random seed
-    let mut rng = rand::thread_rng(); 
     //Randomly choosing a tile number
-    let tile_number = valid_tile_numbers.choose(&mut rng).unwrap();
+    let tile_number = number;
     let idx = ((offset[0]-1)*70 + offset[1]+3) as usize ;
     map.replace_range(idx..(idx+2),&tile_number);
 }
@@ -92,58 +90,6 @@ fn render(map: &String) {
     }
 }
 
-
-/* CODE REPLACED BY ABOVE MATCH STATEMENT
-        //TODO: Replace these with a match statement
-
-        if character == 'S' { //Color the Sheep!
-            let replacement_string = format!("{}",fill_symbol).truecolor(140, 181, 14);
-            print!("{}", replacement_string);}
-
-        if character == 'H' { //Color the Trees!
-            let replacement_string = format!("{}",fill_symbol).truecolor(24, 152, 55);
-            print!("{}", replacement_string);}
-
-        if character == 'A' { //Color the Wheat!
-            let replacement_string = format!("{}",fill_symbol).truecolor(240, 185, 32);
-            print!("{}", replacement_string);}
-
-        if character == 'C' { //Color the Bricks!
-            let replacement_string = format!("{}",fill_symbol).truecolor(223, 97, 40);
-            print!("{}", replacement_string);}
-
-        if character == 'O' { //Color the Ore!
-            let replacement_string = format!("{}",fill_symbol).truecolor(159, 165, 161);
-            print!("{}", replacement_string);}
-        
-        if character == '~' { //Color the ocean!
-            let replacement_string = "~".truecolor(80,174,206);
-            print!("{}", replacement_string);}   
-
-
-        if character == '6' || character == '8' {
-            let replacement_string = format!("{}",character).truecolor(255,0,0);
-            print!("{}", replacement_string);
-        
-        }
-
-        if character == '!' {
-            let replacement_string = format!("{}",'0').truecolor(255,0,0);
-            print!("{}",replacement_string);
-        }
-
-
-
-            // Who cares about the rest, they stay the same...
-        if character != 'S' && character != 'H' && character != 'A' && character != 'C' && character != 'O' && character != '~'
-        && character != '6' && character != '8' && character != '!'{
-            
-           let new_character = format!("{}",character).truecolor(200,200,200);
-           print!("{}",new_character);
-        }
-    }
-}
-*/
 //////////////////////////////////////////////////////////////////////////////////
 
 fn main() {
@@ -156,7 +102,7 @@ fn main() {
     // Reading the map of map.txt into the new string
     file.read_to_string(&mut map).expect("Cannot read the file");
 
-    //Vector containing all [Line, Column] coordinates of the tiles
+    // Vector containing all [Line, Column] coordinates of the tiles
     let tile_coords = vec![
         [10, 30],[13, 22],[13, 38],[16, 14],
         [16, 30],[16, 46],[19, 22],[19, 38],
@@ -164,10 +110,26 @@ fn main() {
         [25, 38],[28, 14],[28, 30],[28, 46],
         [31, 22],[31, 38],[34, 30],
     ];
+    // Vector containing all the possible tile numbers
+    let mut remaining_num = vec!["02","03","03","04","04","05","05","!6","!6","!8","!8","09","09","10","10","11","11","12"];
+    // Vector containing all the possible tile resources
+    let mut remaining_resource = vec!["S","S","S","S","H","H","H","H","A","A","A","A","C","C","C","O","O","O"];
     //Calls color_tile for all 19 tiles on the map.
     for i in 0..19 {
-        color_tile(tile_coords[i], &mut map);
-        enumerate_tile(tile_coords[i], &mut map);
+        // randomizing resource
+        let rng_rsc = rand::thread_rng().gen_range(0..remaining_resource.len());
+        let rsc = remaining_resource[rng_rsc];
+        //creating tile resource
+        color_tile(tile_coords[i], &mut map, String::from(rsc));
+        // removing generated tile from vector
+        remaining_resource.remove(rng_rsc);
+        // randomizing numbers 
+        let rng_num = rand::thread_rng().gen_range(0..remaining_num.len());
+        let num = remaining_num[rng_num];
+        // creating tile number
+        enumerate_tile(tile_coords[i], &mut map, String::from(num));
+        // removing generated tile from vector
+        remaining_num.remove(rng_num);
     }
     //Rendering the final map onto the terminal
     render(&map);
