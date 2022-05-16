e1add5d18d48f95ebf349d14ad570b3b0f95ee3e
Tochi Ukegbu <tochiukegbu@gmail.com>
2022-05-15 21:11:28 -0500

    separated creating render vector
    separated it from processing initializing map data

--- src/main.rs
+++ src/main.rs
@@ -8,22 +8,22 @@ fn main() {
     let mut map_str = String::new();
     file.read_to_string(&mut map_str).expect("Cannot read the file");
 
-    let mut renders = Vec::new();
     let mut line_len = 0;
+
+    let mut offsets : Vec<usize> = Vec::new();
 
     for (i, c) in map_str.chars().enumerate() {
         if c == '[' {
-            renders.push(render::LandRender::from_offset(i));
+            offsets.push(i);
         } else if c == '\n' && line_len == 0 {
             line_len = i;
-
-            // this is fine because we find a \n before we find any other special characters
-            render::set_canvas_width(line_len as i32); 
-
-            // in the future we will first create the canvas object then augmenting it with renders
+            render::set_canvas_width(line_len as i32);
         }
     }
 
     // test
-    render::render(&mut map_str, &mut renders);
+    render::render(
+        &mut map_str, 
+        &mut offsets.iter().map(|&offset| render::LandRender::from_offset(offset)).collect()
+    );
 }
