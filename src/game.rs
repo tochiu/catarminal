ffc887652c3b9a5daa09e9748fa64ce0e3114ec9
Tochi Ukegbu <tochiukegbu@gmail.com>
2022-09-12 12:59:54 -0400

    unfinished interface implementation

--- src/game.rs
+++ src/game.rs
@@ -3,7 +3,6 @@ use crate::{
     render::{
         drag::Dragger,
         space::*,
-        shape::BitShape,
         map::{Map, Tile}, world::World
     }
 };
@@ -13,49 +12,15 @@ use crossterm::{
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
+use log::info;
 use tui_logger::{TuiLoggerWidget, TuiLoggerLevelOutput};
 use std::{io, time::Duration};
 use tui::{
     backend::{CrosstermBackend, Backend},
-    Terminal, widgets::*, layout::{Constraint, Layout, Direction}, style::{Style, Color}, Frame,
+    Terminal, widgets::*, layout::{Constraint, Layout, Direction, Rect}, style::{Style, Color}, Frame,
 };
 
 use rand::Rng;
-
-lazy_static! {
-    static ref DOUBLE_UP_BITSHAPE: BitShape = {
-        BitShape::new(
-            vec![
-                0b000000000111000000000000000000000000000011100000000000001110000000000000000000000000000000000000000000000000000111111100,
-                0b000000000111000000000000000000000000000011100000000000001110000000000000000000000000000000000000000000000000000111111100,
-                0b000111111111000001111100000111000000111011100000000000001110000111111111000000000111000001111011100011111000111100000111,
-                0b111111111111000011111100000111000000111011100011111100001110000111000111000000000111000001111011100011111000000000000111,
-                0b111000000111001110000011100111000000111011111100000011101110111111111111000000000111000001111011111100001110000000111100,
-                0b111000000111001110000011100111000000111011100000000011101110111000000000000000000111000001111011100000001110000111100000,
-                0b111111111111001111111111100111111111111011111111111100001110111111111111000000000111111111111011111111111000000000000000,
-                0b111111111111000001111100000000111111111011111111111100001110000111111111000000000000111111111011100000000000000111100000,
-                0b000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000011100000000000000000000000
-            ],
-            Size2D::new(120, 9)
-        )
-    };
-
-    static ref DND_NOW_BITSHAPE: BitShape = {
-        BitShape::new(
-            vec![
-                0b111111111111100011111100011111101111111111111000000000011111000011111101111111111111111011111100000011111100111111111111,
-                0b111111111111110011111100011111101111111111111100000000011111000011111101111111111111111011111100000011111100111111111111,
-                0b111111000111110011111111011111101111110001111100000000011111110011111101111110000111111011111100000011111100111100011111,
-                0b111111000111110011111111111111101111110001111100000000011111111111111101111110000111111011111101111111111100000001111111,
-                0b111111000111110011111111111111101111110001111100000000011111111111111101111110000111111011111111111111111100000001111110,
-                0b111111000111110011111101111111101111110001111100000000011111001111111101111110000111111011111111111111111100000000000000,
-                0b111111111111110011111100011111101111111111111100000000011111000011111101111111111111111011111111001111111100000001111000,
-                0b111111111111100011111100001111101111111111111000000000011111000001111101111111111111111011111110000111111100000001111000
-            ],
-            Size2D::new(120, 8)
-        )
-    };
-}
 
 pub fn run(enable_logger: bool) -> Result<(), io::Error> {
 
@@ -104,6 +69,7 @@ pub fn run(enable_logger: bool) -> Result<(), io::Error> {
                     }
                 },
                 Event::Mouse(event) => {
+                    info!("mouse event: {:?}", event);
                     should_render = should_render || world.input.handle_mouse_input(event, &mut world.canvas);
                 }
             }
@@ -114,45 +80,84 @@ pub fn run(enable_logger: bool) -> Result<(), io::Error> {
         }
 
         terminal.draw(|f| {
-            draw_frame(f, &mut world, enable_logger);
+            if enable_logger {
+                let rects = Layout::default()
+                    .direction(Direction::Vertical)
+                    .constraints([Constraint::Percentage(80), Constraint::Percentage(20)].as_ref())
+                    .split(f.size());
+                
+                draw_frame(f, rects[0], &mut world);
+                
+                let tui_w = TuiLoggerWidget::default()
+                    .block(
+                        Block::default()
+                            .title(" Log ")
+                            .border_style(Style::default().fg(Color::White).bg(Color::Black))
+                            .borders(Borders::ALL),
+                    )
+                    .output_separator('|')
+                    .output_timestamp(Some("%F %H:%M:%S%.3f ".to_string()))
+                    .output_level(Some(TuiLoggerLevelOutput::Long))
+                    .output_target(false)
+                    .output_file(false)
+                    .output_line(false)
+                    .style_error(Style::default().fg(Color::Red))
+                    .style_debug(Style::default().fg(Color::Cyan))
+                    .style_warn(Style::default().fg(Color::Yellow))
+                    .style_trace(Style::default().fg(Color::White))
+                    .style_info(Style::default().fg(Color::Green));
+
+                f.render_widget(tui_w, rects[1]);
+            } else {
+                draw_frame(f, f.size(), &mut world);
+            }
         })?;
     }
 
     Ok(())
 }
 
-fn draw_frame<B: Backend>(f: &mut Frame<B>, world: &mut World, draw_logger: bool) {
+fn draw_frame<B: Backend>(f: &mut Frame<B>, space: Rect, world: &mut World) {
+    // TODO: refactor this garbage since tui's built in layout system kinda blows
+
+    let rects = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([Constraint::Length(space.width.saturating_sub(39)), Constraint::Min(39)].as_ref())
+        .split(space);
     
+    let event_log = Block::default()
+        .title(" Events ")
+        .border_style(Style::default().fg(Color::White).bg(Color::Black))
+        .borders(Borders::ALL);
+    let chat = Block::default()
+        .title(" Chat ")
+        .border_style(Style::default().fg(Color::White).bg(Color::Black))
+        .borders(Borders::ALL);
+    let players = Block::default()
+        .title(" Players ")
+        .border_style(Style::default().fg(Color::White).bg(Color::Black))
+        .borders(Borders::ALL);
+    
+    let map_block = Block::default()
+        .title(" Map ")
+        .border_style(Style::default().fg(Color::White).bg(Color::Black))
+        .borders(Borders::ALL);
+    let map_rect = map_block.inner(rects[0]);
 
-    // Log output
-    if draw_logger {
-        let rects = Layout::default()
-            .direction(Direction::Vertical)
-            .constraints([Constraint::Percentage(80), Constraint::Percentage(20)].as_ref())
-            .split(f.size());
+    let right_pane_rects = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([Constraint::Length(rects[1].height - 5*4), Constraint::Min(5*4)].as_ref())
+        .split(rects[1]);
+    let chat_event_rects = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
+        .split(right_pane_rects[0]);
 
-        f.render_widget(world.as_widget(), rects[0]);
+    
+    f.render_widget(map_block, rects[0]);
+    f.render_widget(world.as_widget(), map_rect);
 
-        let tui_w = TuiLoggerWidget::default()
-            .block(
-                Block::default()
-                    .title("Log output")
-                    .border_style(Style::default().fg(Color::White).bg(Color::Black))
-                    .borders(Borders::ALL),
-            )
-            .output_separator('|')
-            .output_timestamp(Some("%F %H:%M:%S%.3f ".to_string()))
-            .output_level(Some(TuiLoggerLevelOutput::Long))
-            .output_target(false)
-            .output_file(false)
-            .output_line(false)
-            .style_error(Style::default().fg(Color::Red))
-            .style_debug(Style::default().fg(Color::Cyan))
-            .style_warn(Style::default().fg(Color::Yellow))
-            .style_trace(Style::default().fg(Color::White))
-            .style_info(Style::default().fg(Color::Green));
-        f.render_widget(tui_w, rects[1]);
-    } else {
-        f.render_widget(world.as_widget(), f.size());
-    }
+    f.render_widget(event_log, chat_event_rects[0]);
+    f.render_widget(chat, chat_event_rects[1]);
+    f.render_widget(players, right_pane_rects[1]);
 }
