bdf35d444860300ac926b14d3c234fea90ad566c
Tochi Ukegbu <tochiukegbu@gmail.com>
2022-09-17 15:42:49 -0400

    removfed DrawBuffer trait its superseded by StringShape

--- src/render/draw.rs
+++ src/render/draw.rs
@@ -3,11 +3,6 @@ use super::{
     world::*
 };
 
-use tui::{
-    buffer::Buffer,
-    style::Style
-};
-
 pub trait Layoutable {
     fn layout_ref(&self) -> &DrawLayout;
 }
@@ -53,23 +48,5 @@ impl DrawLayout {
     pub fn set_anchor(&mut self, anchor: Scale2D) -> &mut Self {
         self.space.anchor = anchor;
         self
-    }
-}
-
-pub trait DrawBuffer {
-    fn draw_lines<S>(&mut self, lines: &Vec<S>, draw_space: AbsoluteSpace, full_space: AbsoluteSpace, style: Style)
-        where S: AsRef<str>;
-}
-
-impl DrawBuffer for Buffer {
-    fn draw_lines<S>(&mut self, lines: &Vec<S>, draw_space: AbsoluteSpace, full_space: AbsoluteSpace, style: Style)
-        where S: AsRef<str>
-    {
-        let offset_y = draw_space.position.y - full_space.position.y;
-        let offset_x = draw_space.position.x - full_space.position.x;
-        for y in 0..lines.len().min(draw_space.size.y as usize) as i16 {
-            let point = draw_space.absolute_position_of(Point2D::new(0, y as i16));
-            self.set_stringn(point.x as u16, point.y as u16, &lines[(y + offset_y) as usize].as_ref()[offset_x as usize..], draw_space.size.x as usize, style);
-        }
     }
 }
