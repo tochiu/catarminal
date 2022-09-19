429e82f25f62afc2312167f9dd30516fc1c7c4bc
Tochi Ukegbu <tochiukegbu@gmail.com>
2022-09-19 17:12:46 -0400

    render test code moved to render::run this will be how the game starts the render pipeline

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ mod render;
 mod enums;
 mod game;
 
-fn main() {
+fn main() -> Result<(), std::io::Error> {
     tui_logger::init_logger(log::LevelFilter::Trace).unwrap();
     tui_logger::set_default_level(log::LevelFilter::Trace);
-    game::run(std::env::args().nth(1) == Some(String::from("log"))).unwrap();
+    game::start(std::env::args().nth(1) == Some(String::from("log")))
 }
--- src/render/mod.rs
+++ src/render/mod.rs
@@ -5,4 +5,7 @@ pub mod shape;
 pub mod world;
 pub mod drag;
 
+mod run;
+pub use run::run;
+
 mod mount;
