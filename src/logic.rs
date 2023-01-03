c0cee093a9be76cb153a68633b7152006c879108
BryanTejeda <bryantejeda1738@outlook.com>
2023-01-02 19:15:37 -0500

    Comments and things

--- src/logic.rs
+++ src/logic.rs
@@ -1,2 +1,28 @@
 // logic.rs
 // this module should contain everything related to starting/running the game loop
+
+// I haven't played catan Before Writing Rules and Game information here
+
+// Setting Up The Game
+// Select a Color (White, Blue, Orange, Red)
+// Take: 5 settlements,
+//       4 Cities,
+//       15 Roads
+// To start the game place 2 roads and 1 settlements on the board.
+
+// Tiles occupies should have resource production corresponding to the type of each tile. 
+// Players recieve resources for each terrain hex around starting settlement marked with stars
+
+// Special trait card "Longest Road" and "Largest Army" on reserve
+
+// Turn Overview
+// Oldest Player goes first Options:
+// Roll For Resource Production (Applies to all Players)
+// May trade resource cards with other players and/or use maritime trade.
+// May build roads, settlements, or cities and/or but development cards. 
+// Can Always play a development card during your turn. 
+
+
+
+
+
