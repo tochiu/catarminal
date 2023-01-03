ee185a316ad77f9310869ba51c9d8095a63caf4f
John B Morillo <45088161+jblol17@users.noreply.github.com>
2023-01-02 19:29:43 -0500

    Comments on things that I think need to be done.

--- src/logic.rs
+++ src/logic.rs
@@ -1,2 +1,15 @@
 // logic.rs
 // this module should contain everything related to starting/running the game loop
+
+/*
+    I think we need:
+    Structs to keep track of each player's:
+    Resource numbers
+    Development Cards
+    ID
+    "Color Name"
+    Turn state
+
+
+    Game state struct
+*/
